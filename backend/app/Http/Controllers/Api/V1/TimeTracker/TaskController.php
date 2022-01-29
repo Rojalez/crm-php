00201b6df35fc23eb044bf9bdf8c8bfbe23cddd1
beknazar312 <beknazarnurkulov@gmail.com>
2022-01-29 16:25:33 +0600

    add task controller

---
+++ backend/app/Http/Controllers/Api/V1/TimeTracker/TaskController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\TimeTracker;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::all();
+
+        return $tasks;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $task = Task::create([
+            'title' => $request->title,
+            'executor_id' => $request->executor_id,
+            'text' => $request->text,
+            'user_id' => $request->user_id,
+        ]);
+
+        return json_encode($task);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $task = Task::findOrFail($id);
+
+        return $task;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->title = $request->title;
+        $task->text = $request->text;
+        $task->user_id = $request->user_id;
+        $task->executor_id = $request->executor_id;
+        $task->save();
+
+        return $task;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return ['success'];
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('register', 'Auth\PassportAuthController@register');
-Route::post('login', 'PassportAuthController@login');
+Route::post('login', 'Auth\PassportAuthController@login');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -29,6 +28,58 @@ Route::middleware('auth:api')->group(function () {
         ]);
     });
 });
+
+Route::get('/users', function () {
+    $users = [
+        [
+            'name' => 'Sultan',
+            'age' => '24'
+        ],
+        [
+            'name' => 'Beknazar',
+            'age' => '26'
+        ]
+    ];
+    return json_encode($users);
+});
+
+Route::get('/user/{id}/{test}/', function ($id, $test) {
+    $users = [
+        [
+            'name' => 'Sultan',
+            'age' => '24'
+
+        ],
+        [
+            'name' => 'Beknazar',
+            'age' => '26'
+        ]
+    ];
+    return json_encode($users[$id][$test]);
+});
+
+Route::get('/', function ($id, $test) {
+    $users = [
+        [
+            'name' => 'Sultan',
+            'age' => '24'
+
+        ],
+        [
+            'name' => 'Beknazar',
+            'age' => '26'
+        ]
+    ];
+    return json_encode($users[$id][$test]);
+});
+
+Route::prefix('time-tracker')->group(function () {
+    Route::apiResources([
+        'task' => \TimeTracker\TaskController::class,
+    ]);
+});
+
+
 
 
 
