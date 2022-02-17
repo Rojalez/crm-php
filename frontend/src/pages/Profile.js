dd5fcd672a1de49cc54339cb232181a3b7f94326
Sultanbek <sbakirov97@gmail.com>
2022-02-18 01:47:24 +0600

    cant get profile id

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,18 +2,27 @@ import React, {useState, useEffect} from "react";
 import { getUserById } from "../API/UserService";
 import { useFetching } from "../hooks/useFetching";
 import useHeader from "../hooks/useHeader";
-const Profile = ({id}) => {
+const Profile = () => {
     const [user, setUser] = useState([])
     const header = useHeader()
-    const [fetchTaskById, isLoading, error] = useFetching(async (id) => {
+    const id = user.id
+    const [subscribe, setSubscribe] = useState(false)
+    const [fetchTaskById, isLoading, error] = useFetching(async () => {
         const response = await getUserById(header, id);
         setUser(response.data);
         console.log(response.data)
     });
 
     useEffect(() => {
+        setSubscribe(true)
         fetchTaskById(user.id)
+        return () => setSubscribe(false)
     }, [])
+
+    if(!subscribe) {
+        return null;
+    }
+    
     return (
         <div className="my-20">
          
